d45cba8d8ca4442a4dbc2f08d8fe51d4baaef34c
msfom <marinafomina1994@gmail.com>
2022-12-27 17:51:11 +0300

    Сделано ДЗ. Подключение библиотек

--- src/main/java/ru/fomina/recipes/controller/FirstController.java
+++ src/main/java/ru/fomina/recipes/controller/FirstController.java
@@ -1,18 +1,23 @@
 package ru.fomina.recipes.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@Tag(name = "Вводные данные о проекте")
 public class FirstController {
 
     @GetMapping
+    @Operation(summary = "Проверка запуска приложения")
     public String runApp() {
         return "Приложение запущено";
     }
 
     @GetMapping("/info")
+    @Operation(summary = "Описание проекта")
     public String description(){
         return "Вы находитесь на странице приложения с рецептами." +
                 " Проект создан 15 декабря 2022 года." +
